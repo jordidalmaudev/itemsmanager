b47ae13f0806482ff50f4a5379ca10f8187c86b6
Jordi Dalmau <jordidalmau.alum@insestatut.cat>
2025-01-04 21:02:22 +0100

    Enhance item rendering logic to display a message when no items are available and update the link to the add item page

--- classes/GestorItems.js
+++ classes/GestorItems.js
@@ -49,32 +49,39 @@ export class GestorItems {
 
     // renderizar item en el html id items desde el localstorage
     renderItems() {
+        console.log(this.items);
+        
         let items = JSON.parse(localStorage.getItem("items"));
         let table = document.getElementById("items");
 
-        items?.forEach(item => {
-            let row = document.createElement('tr');
-            row.innerHTML = `
-                <td>
-                    <img src="${item.urlImage || this.noImg}" width="100" height="100">
-                </td>
-                <td>${item.nom}</td>
-                <td>${item.descripcio}</td>
-                <td>${item.dataCreacio}</td>
-                <td>${item.dataModificacio || ''}</td>
-                <td>
-                    <button class="edit-btn">Editar</button>
-                    <button class="delete-btn">${item.id}</button>
-                </td>
-            `;
-            table?.appendChild(row);
+        if (this.items.length > 0) {
+            items?.forEach(item => {
+                let row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>
+                        <img src="${item.urlImage || this.noImg}" width="100" height="100">
+                    </td>
+                    <td>${item.nom}</td>
+                    <td>${item.descripcio}</td>
+                    <td>${item.dataCreacio}</td>
+                    <td>${item.dataModificacio || ''}</td>
+                    <td>
+                        <button class="edit-btn">Editar</button>
+                        <button class="delete-btn">${item.id}</button>
+                    </td>
+                `;
+                table?.appendChild(row);
 
-            // Agregar event listeners a los botones
-            row.querySelector('.edit-btn').addEventListener('click', () => this.guardarItemYRedirigir(item.id));
-            row.querySelector('.delete-btn').addEventListener('click', () => this.eliminarItems(item.id));
-        });
+                // Agregar event listeners a los botones
+                row.querySelector('.edit-btn').addEventListener('click', () => this.guardarItemYRedirigir(item.id));
+                row.querySelector('.delete-btn').addEventListener('click', () => this.eliminarItems(item.id));
+            });
+        } else {
+            console.log("No hay items");
+            
+            table.innerHTML = `<h1>No hay items</h1>`;
+            return;
+        }
     }
 }
 
-const gestor = new GestorItems();
-gestor.renderItems();
--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 import { GestorItems } from "../classes/GestorItems.js";  
-import { Item } from "../classes/Item.js";
-import { ItemVisual } from "../classes/ItemVisual.js";
+// import { Item } from "../classes/Item.js";
+// import { ItemVisual } from "../classes/ItemVisual.js";
 
-let newItem = new GestorItems();
+const gestor = new GestorItems();
+gestor.renderItems();
+// let pcgaming = new Item("PC Gaming", "Pc del tipus gaming");
+// newItem.afegirItems(pcgaming);
+// let pcnormal = new Item("PC Normal", "Pc del tipus normal");
+// newItem.afegirItems(pcnormal);
 
-let pcgaming = new Item("PC Gaming", "Pc del tipus gaming");
-newItem.afegirItems(pcgaming);
-let pcnormal = new Item("PC Normal", "Pc del tipus normal");
-newItem.afegirItems(pcnormal);
+// let pcw = new ItemVisual("PC Workstation", "Pc del tipus workstation", "./img/worstation.webp");
+// newItem.afegirItems(pcw);
 
-let pcw = new ItemVisual("PC Workstation", "Pc del tipus workstation", "./img/worstation.webp");
-newItem.afegirItems(pcw);
+// let pcw2 = new ItemVisual();
+// newItem.afegirItems(pcw2);
 
-let pcw2 = new ItemVisual();
-newItem.afegirItems(pcw2);
-
-newItem.renderItems();
+// newItem.renderItems();
